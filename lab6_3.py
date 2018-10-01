f5315771af8139b41b4acb6b26ecdf43aceecde1
HrytskivB <32675472+HrytskivB@users.noreply.github.com>
2018-10-01 12:19:14 +0300

    Create lab6_3.py

---
+++ lab6_3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import decimal
+
+def ui_input() -> list:
+    """Input of argument"""
+    
+    cur_money = decimal.Decimal(input("Input current amount of money: "))
+    percent = decimal.Decimal(input("Input annual interest rate of the bank: "))
+    final_money = decimal.Decimal(input("Input final amount of money: "))
+    
+    return [cur_money, percent, final_money]
+
+def ui_output(result: str) -> None:
+    """Output of result"""
+    
+    result = str(result)
+    print("Duration of deposit: ", result)
+
+def deposit_duration_calculate(deposit_data: list) -> float:
+    """Calcucate duration of deposit"""
+    
+    if len(deposit_data) != 3:
+        print("Too many or less members of list")
+        exit()
+    
+    #TODO duration
+    duration = 30
+    
+    return duration
+
+ui_output(deposit_duration_calculate(ui_input()))
